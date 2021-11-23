7e9db8fc8235f018e7975bcc49bad4ac84d32296
VladislavaKaliuta <vlada18.02@mail.ru>
2021-11-23 21:45:27 +0300

    Refactoring

--- Insertion_sort/Insertion_sort/main.cpp
+++ Insertion_sort/Insertion_sort/main.cpp
@@ -9,24 +9,37 @@
 
 using namespace std;
 
+template <typename T>
+T GetCorrectNumber(T min, T max)
+{
+    T x;
+    while ((std::cin >> x).fail() || x < min || x > max)
+    {
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        std::cout << "Enter number (" << min << "-" << max <<") again:";
+    }
+    return x;
+    
+}
 
 int i, j, key = 0, temp = 0;
 void InsertionSort(int *mas, int n) //сортировка вставками
 {
     for (i = 0; i < n - 1; i++)
     {
-        key = i + 1; // наш указатель переводим на первый элемент, так как в цикле мы идём от 0
-        temp = mas[key]; //temp - это тот элемент, который мы будем пробовать "вставлять"
+        key = i + 1; // наш указатель переводим на второй элемент (индексация массива начинается с 0)
+        temp = mas[key]; //temp - элемент, который мы будем "вставлять"
         for (j = i + 1; j > 0; j--)
         {
-            if (temp < mas[j - 1]) // если тот элемент, который мы хотим переставить меньше элемента, расположенного слева от него, то
+            if (temp < mas[j - 1]) // если тот элемент, который мы хотим переставить, меньше элемента, расположенного слева от него, то
             {
-                mas[j] = mas[j - 1]; // меняем их местами
-                key = j - 1; // указатель сдвигаем, и он выйдет в отрицательный диапозон, вследствие чего цикл прервётся
+                mas[j] = mas[j - 1]; // элемент слева от temp сдвинули вправо
+                key = j - 1; // указатель обнуляем, вследствие чего цикл прервётся
             }
  
         }
-        mas[key] = temp;
+        mas[key] = temp; // temp сдвинули влево
     }
     cout << endl << "Отсортированный массив массив: ";
     for (i = 0; i < n; i++) // вывод массива
@@ -39,8 +52,7 @@ int main()
     setlocale(LC_ALL, "Rus");
         
     cout << "Количество элементов в массиве: " << "";
-    int n;
-        cin >> n;
+    int n = GetCorrectNumber(1, 100);
         int *mas = new int[n];
         for (i = 0; i < n; i++) //ввод исходного массива
         {
@@ -49,7 +61,6 @@ int main()
         }
         InsertionSort(mas, n); //вызов функции
         cout << endl;
-        //system("pause");
    
     return 0;
 }
